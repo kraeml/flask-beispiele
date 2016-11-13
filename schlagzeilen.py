fde65ca913de16438e2b15c097e3ee5475f740d2
kraeml <kraeml@sesamestreet.franken.de>
2016-11-13 20:19:48 +0100

    Ausgabe mit Openweathermap.org

--- schlagzeilen.py
+++ schlagzeilen.py
@@ -2,6 +2,10 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
+import urllib2
+import urllib
+
 
 
 app = Flask(__name__)
@@ -11,15 +15,29 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def get_news():
-    query = request.form.get("publication")
+    query = request.args.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    weather = get_weather("Nuremberg, DE")
+    return render_template("home.html", articles=feed['entries'], weather=weather)
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=YOUR_ID"
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description": parsed["weather"][0]["description"],
+                    "temperature":parsed["main"]["temp"],
+                    "city":parsed["name"]}
+    return weather
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host="0.0.0.0")
