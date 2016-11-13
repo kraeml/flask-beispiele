e29611cb817eae6f24a08418cbb97027a46525d4
kraeml <kraeml@sesamestreet.franken.de>
2016-11-13 20:33:46 +0100

    Eingabe mit suche der Stadt

--- schlagzeilen.py
+++ schlagzeilen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import feedparser
 from flask import Flask
 from flask import render_template
@@ -6,7 +8,7 @@ import json
 import urllib2
 import urllib
 
-
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=YourID"
 
 app = Flask(__name__)
 
@@ -15,29 +17,43 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+DEFAULTS = {'publication':'bbc',
+            'city': 'London,UK'}
+
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
+def home():
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles, weather=weather)
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS["publication"]
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    weather = get_weather("Nuremberg, DE")
-    return render_template("home.html", articles=feed['entries'], weather=weather)
+    return feed['entries']
 
 def get_weather(query):
-    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=YOUR_ID"
     query = urllib.quote(query)
-    url = api_url.format(query)
+    url = WEATHER_URL.format(query)
     data = urllib2.urlopen(url).read()
     parsed = json.loads(data)
     weather = None
-    if parsed.get("weather"):
-        weather = {"description": parsed["weather"][0]["description"],
-                    "temperature":parsed["main"]["temp"],
-                    "city":parsed["name"]}
+    if parsed.get('weather'):
+        weather = {'description':parsed['weather'][0]['description'],
+                    'temperature':parsed['main']['temp'],
+                    'city':parsed['name']}
     return weather
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host="0.0.0.0")
