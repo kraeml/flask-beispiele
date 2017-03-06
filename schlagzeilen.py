dd02f5021b3bc22e6cfed606c43f014dbf926613
Schuljahr 2016/17 <SJ201617@example.com>
2017-03-06 21:08:03 +0100

    Writing the fallback logic to check for cookies

--- schlagzeilen.py
+++ schlagzeilen.py
@@ -31,7 +31,9 @@ def home():
     # get customized headlines, based on user input or default
     publication = request.args.get('publication')
     if not publication:
-        publication = DEFAULTS['publication']
+        publication = request.cookies.get("publication")
+        if not publication:
+            publication = DEFAULTS['publication']
     articles = get_news(publication)
     # get customized weather based on user input or default
     city = request.args.get('city')
